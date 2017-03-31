7bcd3c6fa68b52c8312d21909ab31a9831cecc67
kanhua <kanhwa@gmail.com>
2017-03-31 16:35:40 +0900

    add Pipeline

--- image_process.py
+++ image_process.py
@@ -1,14 +1,89 @@
 import numpy as np
 import cv2
 import glob
+import os
+import pickle
 import matplotlib.pyplot as plt
 
 
+
 class CameraCalibrator(object):
+    def __init__(self, img_size, img_folder, nx, ny, obj_img_cache="obj_img_cache.p",recalc=False):
+        self.img_size = img_size
 
-    def __init__(self):
-        pass
+        if os.path.exists(obj_img_cache)==True and recalc==False:
+            cache=pickle.load(open(obj_img_cache,'rb'))
+            self.objpoints=cache['objpoints']
+            self.imgpoints=cache['imgpoints']
 
+        else:
+            self.objpoints, self.imgpoints = get_calibration_factors(image_folder=img_folder,
+                                                                     nx=nx, ny=ny)
+            with open(obj_img_cache,'wb') as fp:
+                pickle.dump({'objpoints':self.objpoints,'imgpoints':self.imgpoints},fp)
+
+
+        self.retval, self.cameraMatrix, \
+        self.distCoeffs, self.rvecs, self.tvecs = cv2.calibrateCamera(self.objpoints,
+                                                                      self.imgpoints, self.img_size, None, None)
+
+    def transform(self, img):
+        assert get_cv2_img_size(img) == self.img_size
+        undist_img = cv2.undistort(img, self.cameraMatrix, self.distCoeffs, None, self.cameraMatrix)
+
+        return undist_img
+
+    def fit(self,X,y):
+
+        return self
+
+
+class PerspectiveTransformer(object):
+    def __init__(self, src=None, dst=None):
+
+        default_src = [
+            (250, 686),  # left, bottom
+            (583, 458),  # left, top
+            (700, 458),  # right, top
+            (1060, 686)]  # right, bottom
+
+        default_dst = [
+            [300, 720],
+            [300, 0],
+            [800, 0],
+            [800, 720]
+        ]
+
+        if src is None:
+            self.src = np.array(default_src).astype(np.float32)
+        else:
+            self.src = src
+
+        if dst is None:
+            self.dst = np.array(default_dst).astype(np.float32)
+        else:
+            self.dst = dst
+
+    def transform(self, img):
+        return warper(img, self.src, self.dst)
+
+    def fit(self,X,y):
+        return self
+
+    def get_M(self):
+        M = cv2.getPerspectiveTransform(self.src, self.dst)
+        return M
+
+    def get_invM(self):
+        invM=cv2.getPerspectiveTransform(self.dst,self.src)
+        return invM
+
+
+
+def get_cv2_img_size(img):
+    assert isinstance(img, np.ndarray)
+
+    return img[:, :, 0].T.shape[:2]
 
 
 
@@ -20,16 +95,17 @@ def mag_thresh(img, sobel_kernel=3, mag_thresh=(0, 255)):
     sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
     sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
     # Calculate the gradient magnitude
-    gradmag = np.sqrt(sobelx**2 + sobely**2)
+    gradmag = np.sqrt(sobelx ** 2 + sobely ** 2)
     # Rescale to 8 bit
-    scale_factor = np.max(gradmag)/255
-    gradmag = (gradmag/scale_factor).astype(np.uint8)
+    scale_factor = np.max(gradmag) / 255
+    gradmag = (gradmag / scale_factor).astype(np.uint8)
     # Create a binary image of ones where threshold is met, zeros otherwise
     binary_output = np.zeros_like(gradmag)
     binary_output[(gradmag >= mag_thresh[0]) & (gradmag <= mag_thresh[1])] = 1
 
     # Return the binary image
     return binary_output
+
 
 def abs_sobel_thresh(img, orient='x', thresh_min=0, thresh_max=255):
     # Convert to grayscale
@@ -41,7 +117,7 @@ def abs_sobel_thresh(img, orient='x', thresh_min=0, thresh_max=255):
     if orient == 'y':
         abs_sobel = np.absolute(cv2.Sobel(gray, cv2.CV_64F, 0, 1))
     # Rescale back to 8 bit integer
-    scaled_sobel = np.uint8(255*abs_sobel/np.max(abs_sobel))
+    scaled_sobel = np.uint8(255 * abs_sobel / np.max(abs_sobel))
     # Create a copy and apply the threshold
     binary_output = np.zeros_like(scaled_sobel)
     # Here I'm using inclusive (>=, <=) thresholds, but exclusive is ok too
@@ -51,8 +127,7 @@ def abs_sobel_thresh(img, orient='x', thresh_min=0, thresh_max=255):
     return binary_output
 
 
-
-def dir_threshold(img, sobel_kernel=3, thresh=(0, np.pi/2)):
+def dir_threshold(img, sobel_kernel=3, thresh=(0, np.pi / 2)):
     # Grayscale
     gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     # Calculate the x and y gradients
@@ -61,29 +136,29 @@ def dir_threshold(img, sobel_kernel=3, thresh=(0, np.pi/2)):
     # Take the absolute value of the gradient direction,
     # apply a threshold, and create a binary image result
     absgraddir = np.arctan2(np.absolute(sobely), np.absolute(sobelx))
-    binary_output =  np.zeros_like(absgraddir)
+    binary_output = np.zeros_like(absgraddir)
     binary_output[(absgraddir >= thresh[0]) & (absgraddir <= thresh[1])] = 1
 
     # Return the binary image
     return binary_output
 
+
 def hls_select(img, thresh=(0, 255)):
     hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
-    s_channel = hls[:,:,2]
+    s_channel = hls[:, :, 2]
     binary_output = np.zeros_like(s_channel)
     binary_output[(s_channel > thresh[0]) & (s_channel <= thresh[1])] = 1
     return binary_output
 
 
-def get_calibration_factors(image_folder='./camera_cal/calibration*.jpg',nx=9,ny=5):
-
+def get_calibration_factors(image_folder='./camera_cal/calibration*.jpg', nx=9, ny=5):
     # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-    objp = np.zeros((ny*nx,3), np.float32)
-    objp[:,:2] = np.mgrid[0:nx, 0:ny].T.reshape(-1,2)
+    objp = np.zeros((ny * nx, 3), np.float32)
+    objp[:, :2] = np.mgrid[0:nx, 0:ny].T.reshape(-1, 2)
 
     # Arrays to store object points and image points from all the images.
-    objpoints = [] # 3d points in real world space
-    imgpoints = [] # 2d points in image plane.
+    objpoints = []  # 3d points in real world space
+    imgpoints = []  # 2d points in image plane.
 
     # Make a list of calibration images
     images = glob.glob(image_folder)
@@ -94,7 +169,7 @@ def get_calibration_factors(image_folder='./camera_cal/calibration*.jpg',nx=9,ny
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
         # Find the chessboard corners
-        ret, corners = cv2.findChessboardCorners(gray, (nx,ny), None)
+        ret, corners = cv2.findChessboardCorners(gray, (nx, ny), None)
 
         # If found, add object points, image points
         if ret == True:
@@ -102,19 +177,35 @@ def get_calibration_factors(image_folder='./camera_cal/calibration*.jpg',nx=9,ny
             imgpoints.append(corners)
 
             # Draw and display the corners
-            cv2.drawChessboardCorners(img, (nx,ny), corners, ret)
-            #write_name = 'corners_found'+str(idx)+'.jpg'
-            #cv2.imwrite(write_name, img)
+            cv2.drawChessboardCorners(img, (nx, ny), corners, ret)
+            # write_name = 'corners_found'+str(idx)+'.jpg'
+            # cv2.imwrite(write_name, img)
 
-    return objpoints,imgpoints
+    return objpoints, imgpoints
+
 
 def cal_undistort(img, objpoints, imgpoints):
     # Use cv2.calibrateCamera() and cv2.undistort()
-    retval, cameraMatrix, distCoeffs, rvecs, tvecs=cv2.calibrateCamera(objpoints,imgpoints,img[:,:,0].T.shape[:2],None,None)
-    undist=cv2.undistort(img,cameraMatrix,distCoeffs,None,cameraMatrix)
-    #undist = np.copy(img)  # Delete this line
+    retval, cameraMatrix, distCoeffs, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, img[:, :, 0].T.shape[:2],
+                                                                         None, None)
+    undist = cv2.undistort(img, cameraMatrix, distCoeffs, None, cameraMatrix)
+    # undist = np.copy(img)  # Delete this line
     return undist
 
+class EdgeExtractor(object):
+
+    def __init__(self,s_thresh=(100, 255), sx_thresh=(50, 200)):
+        self.s_thresh=s_thresh
+        self.sx_thresh=sx_thresh
+
+    def transform(self,img):
+
+        _,n_img=edge_pipeline(img,self.s_thresh,self.sx_thresh)
+
+        return n_img
+
+    def fit(self,X,y):
+        return self
 
 def edge_pipeline(img, s_thresh=(100, 255), sx_thresh=(50, 200)):
     img = np.copy(img)
@@ -139,14 +230,13 @@ def edge_pipeline(img, s_thresh=(100, 255), sx_thresh=(50, 200)):
     # be beneficial to replace this channel with something else.
     color_binary = np.dstack((np.zeros_like(sxbinary), sxbinary, s_binary))
 
-    single_channel_binary=np.zeros_like(scaled_sobel)
-    single_channel_binary[(s_binary==1) | (sxbinary==1) ]=1
+    single_channel_binary = np.zeros_like(scaled_sobel)
+    single_channel_binary[(s_binary == 1) | (sxbinary == 1)] = 1
 
-    return color_binary,single_channel_binary
+    return color_binary, single_channel_binary
 
 
 def warper(img, src, dst):
-
     # Compute and apply perpective transform
     img_size = (img.shape[1], img.shape[0])
     M = cv2.getPerspectiveTransform(src, dst)
@@ -154,21 +244,90 @@ def warper(img, src, dst):
 
     return warped
 
+def find_lane_points(binary_warped):
+    
+    # Assuming you have created a warped binary image called "binary_warped"
+    # Take a histogram of the bottom half of the image
+    histogram = np.sum(binary_warped[int(binary_warped.shape[0] / 2):, :], axis=0)
+    # Create an output image to draw on and  visualize the result
+    out_img = np.dstack((binary_warped, binary_warped, binary_warped)) * 255
+    # Find the peak of the left and right halves of the histogram
+    # These will be the starting point for the left and right lines
+    midpoint = np.int(histogram.shape[0] / 2)
+    leftx_base = np.argmax(histogram[:midpoint])
+    rightx_base = np.argmax(histogram[midpoint:]) + midpoint
 
-if __name__=="__main__":
+    # Choose the number of sliding windows
+    nwindows = 9
+    # Set height of windows
+    window_height = np.int(binary_warped.shape[0] / nwindows)
+    # Identify the x and y positions of all nonzero pixels in the image
+    nonzero = binary_warped.nonzero()
+    nonzeroy = np.array(nonzero[0])
+    nonzerox = np.array(nonzero[1])
+    # Current positions to be updated for each window
+    leftx_current = leftx_base
+    rightx_current = rightx_base
+    # Set the width of the windows +/- margin
+    margin = 100
+    # Set minimum number of pixels found to recenter window
+    minpix = 50
+    # Create empty lists to receive left and right lane pixel indices
+    left_lane_inds = []
+    right_lane_inds = []
 
-    objpoints,imgpoints=get_calibration_factors()
-    img=cv2.imread('./camera_cal/calibration1.jpg')
-    img_size=img.shape[1],img.shape[0]
-    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, img_size,None,None)
+    # Step through the windows one by one
+    for window in range(nwindows):
+        # Identify window boundaries in x and y (and right and left)
+        win_y_low = binary_warped.shape[0] - (window + 1) * window_height
+        win_y_high = binary_warped.shape[0] - window * window_height
+        win_xleft_low = leftx_current - margin
+        win_xleft_high = leftx_current + margin
+        win_xright_low = rightx_current - margin
+        win_xright_high = rightx_current + margin
+        # Draw the windows on the visualization image
+        cv2.rectangle(out_img, (win_xleft_low, win_y_low), (win_xleft_high, win_y_high), (0, 255, 0), 2)
+        cv2.rectangle(out_img, (win_xright_low, win_y_low), (win_xright_high, win_y_high), (0, 255, 0), 2)
+        # Identify the nonzero pixels in x and y within the window
+        good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) \
+                          & (nonzerox >= win_xleft_low) & (nonzerox < win_xleft_high)).nonzero()[0]
+        good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) \
+                           & (nonzerox >= win_xright_low) & (nonzerox < win_xright_high)).nonzero()[0]
+        # Append these indices to the lists
+        left_lane_inds.append(good_left_inds)
+        right_lane_inds.append(good_right_inds)
+        # If you found > minpix pixels, recenter next window on their mean position
+        if len(good_left_inds) > minpix:
+            leftx_current = np.int(np.mean(nonzerox[good_left_inds]))
+        if len(good_right_inds) > minpix:
+            rightx_current = np.int(np.mean(nonzerox[good_right_inds]))
 
-    img=cal_undistort(img,objpoints,imgpoints)
+    # Concatenate the arrays of indices
+    left_lane_inds = np.concatenate(left_lane_inds)
+    right_lane_inds = np.concatenate(right_lane_inds)
 
-    cv2.imwrite("test_out.png",img)
+    # Extract left and right line pixel positions
+    leftx = nonzerox[left_lane_inds]
+    lefty = nonzeroy[left_lane_inds]
+    rightx = nonzerox[right_lane_inds]
+    righty = nonzeroy[right_lane_inds]
 
-    #plt.imshow(img)
-    #plt.show()
+    # Fit a second order polynomial to each
+    left_fit = np.polyfit(lefty, leftx, 2)
+    right_fit = np.polyfit(righty, rightx, 2)
+
+    return leftx,lefty,left_fit,rightx,righty,right_fit,out_img,nonzerox,nonzeroy,left_lane_inds,right_lane_inds
 
 
+if __name__ == "__main__":
+    objpoints, imgpoints = get_calibration_factors()
+    img = cv2.imread('./camera_cal/calibration1.jpg')
+    img_size = img.shape[1], img.shape[0]
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, img_size, None, None)
 
+    img = cal_undistort(img, objpoints, imgpoints)
 
+    cv2.imwrite("test_out.png", img)
+
+    # plt.imshow(img)
+    # plt.show()
